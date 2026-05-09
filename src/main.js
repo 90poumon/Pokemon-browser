0034e7bae7dbcbb31e1134ba41539491fb8b544d
Yao Benoit <yaobenoit.makouhoui@student.swic.edu>
2026-05-09 03:44:07 -0500

    feat: Initialize DOM element selection

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+// Select DOM elements
+const pokemonImage = document.querySelector("#pokemon-image");
+const pokemonName = document.querySelector("#pokemon-name");
+const pokemonTypes = document.querySelector("#pokemon-types");
+
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+
+// Current pokemon ID
+const currentId = 1;
+
+// Async function to fetch pokemon
+async function getPokemon(id) {
+
+ 
+
+    const resp = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${id}`
+    ); 
